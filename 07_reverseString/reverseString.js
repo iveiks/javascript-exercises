1b3c43079c0d0408fc18a6a486684eb6f23eeead
Veiko Lemberg <iveiks@gmail.com>
2025-08-31 11:04:25 +0300

    Completed the exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const newString = string.split('');
+  let revString = '';
+  for (i = newString.length; i > 0; i--) {
+    revString += newString.splice(-1);
+  }
+  return revString;
 };
 
 // Do not edit below this line
