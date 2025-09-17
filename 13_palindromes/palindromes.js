b757eb0a5220e36cce2fa8d01314ac25606c5906
iveiks <iveiks@gmail.com>
2025-09-17 20:19:08 +0300

    Palindrome function works with one word

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+  // Build two arrays from the string, one in reverse
+  let arr = string.split('');
+  let arrRev = string.split('').reverse();
+  
+  // Compare the two arrays, if one array position
+  // doesn't match, return false
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != arrRev[i]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 };
 
 // Do not edit below this line
