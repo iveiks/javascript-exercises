53e7a314f21d15882a042440edcb6ee14f950482
iveiks <iveiks@gmail.com>
2025-09-17 21:50:40 +0300

    edited palindromes.js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,9 +1,11 @@
 const palindromes = function (string) {
-
   // Build two arrays from the string, one in reverse
   let arr = string.split('');
   let arrRev = string.split('').reverse();
-  
+
+  // TODO: Add and array that contains the alphabet?
+  // TODO: Filter out non-characters from both arrays
+
   // Compare the two arrays, if one array position
   // doesn't match, return false
   for (let i = 0; i < arr.length; i++) {
